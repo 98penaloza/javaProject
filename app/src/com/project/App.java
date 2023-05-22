856a058ca4fc8b871b5993771658e092b8dcbc45
98penaloza <98penaloza@gmail.com>
2023-05-21 23:58:45 -0700

    Implementing node concepts

--- app/src/com/project/App.java
+++ app/src/com/project/App.java
@@ -3,12 +3,23 @@
  */
 package com.project;
 
+import java.util.Arrays;
+import java.util.List;
+
+import com.project.node.Node;
+import com.project.node.StandardNode;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
+        Node node1 = StandardNode.builder().value(1).build();
+        Node node2 = new StandardNode(2);
+        Node<Object> node3 = new StandardNode(3, null);
         System.out.println(new App().getGreeting());
+        Arrays.asList(node1, node2, node3)
+                .forEach(n -> System.out.println(n.getValue()));
     }
 }
