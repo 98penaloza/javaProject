ef5489338a471419031a7ed41e46b6af8641acea
monterrozagera <monterrozagerardo@protonmail.com>
2023-05-26 00:35:25 -0700

    start double linked list

--- app/src/com/project/linkedlist/StandardDoubleLinkedList.java
+++ app/src/com/project/linkedlist/StandardDoubleLinkedList.java
@@ -1,15 +1,44 @@
 package com.project.linkedlist;
 
 import com.project.node.Node;
-import com.project.node.StandardNode;
+import com.project.node.StandardBidirectionalNode;
+
+import lombok.Getter;
 
 public class StandardDoubleLinkedList<T> implements LinkedList<T> {
     private Node<T> head = null;
 
+    @Getter
+    private int length = 0;
+
+    public StandardDoubleLinkedList() {
+        head = null;
+    }
+
     @Override
-    public void add(Object value) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    public void add(T value) {
+        
+        Node<T> newNode = new StandardBidirectionalNode<>(value);
+        if (head == null) {
+            head = newNode;
+            length = 1;
+        } else {
+            // current node
+            Node<T> current = head;
+            Node<T> prev = null;
+
+            // go through all of em
+            while (current.getNext() != null) {
+                prev = current;
+                current = current.getNext();
+            }
+            
+            // TODO: ask whats up lol
+            // set next node
+            //current.prev = prev;
+            // current.setNext(newNode);
+            length += 1;
+        }
     }
 
     @Override
